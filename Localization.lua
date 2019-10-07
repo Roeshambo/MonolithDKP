32b803b2b5b71e524e79725832a409e506f7aa1a
Roeshambo <joshua_stack@yahoo.com>
2019-10-07 00:44:08 -0500

    Relocated Localization Files

--- Localization.lua
+++
@@ -1,9 +0,0 @@
-local _, core = ...;
-local _G = _G;
-local MonDKP = core.MonDKP;
-
-core.LocalClass = {}
-
-FillLocalizedClassList(core.LocalClass)
-
--- more to be added for tooltips and descriptions
