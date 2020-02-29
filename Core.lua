13e81c19e10e0789a75ba458e78708a2c04d0b6f
lantisnt <lantisnt@github.com>
2020-02-29 17:48:56 +0100

    Add essential versioning to force this version in guild.

--- Core.lua
+++ Core.lua
@@ -88,8 +88,8 @@ core.EncounterList = {      -- Event IDs must be in the exact same order as core
 }
 
 core.MonDKPUI = {}        -- global storing entire Configuration UI to hide/show UI
-core.MonVersion = "v2.1.2";
-core.BuildNumber = 20102;
+core.MonVersion = "vE.0.0";
+core.BuildNumber = 140000;
 core.TableWidth, core.TableRowHeight, core.TableNumRows = 500, 18, 27; -- width, row height, number of rows
 core.SelectedData = { player="none"};         -- stores data of clicked row for manipulation.
 core.classFiltered = {};   -- tracks classes filtered out with checkboxes
