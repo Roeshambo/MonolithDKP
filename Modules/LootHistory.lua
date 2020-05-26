9454c73e21a17c4cc5584bf05200c9a2b323c849
Taidtuskecyh <Taidtuskecyh@gmail.com>
2020-05-26 22:48:54 +0200

    further fixes, on mouseover row colors were missing

--- Modules/LootHistory.lua
+++ Modules/LootHistory.lua
@@ -555,7 +555,7 @@ function MonDKP:LootHistory_Update(filter)				-- if "filter" is included in call
 
 		    	local awarded_by_search = MonDKP:Table_Search(MonDKP_DKPTable, awardOfficer, "player")
 		    	if awarded_by_search then
-			     	c = MonDKP:GetCColors(MonDKP_DKPTable[awarded_by_search[1][1]].class)
+			     	c = MonDKP:GetCColors(MonDKP:GetTable(MonDKP_DKPTable, true)[awarded_by_search[1][1]].class)
 			    else
 			     	c = { hex="444444" }
 			    end
@@ -586,11 +586,11 @@ function MonDKP:LootHistory_Update(filter)				-- if "filter" is included in call
 		    		for j=1, #path do
 		    			local col;
 		    			local bidder = path[j].bidder
-		    			local s = MonDKP:Table_Search(MonDKP_DKPTable, bidder)
+		    			local s = MonDKP:Table_Search(MonDKP:GetTable(MonDKP_DKPTable, true), bidder)
 		    			local path2 = path[j].bid or path[j].dkp or path[j].roll
 
 		    			if s then
-		    				col = MonDKP:GetCColors(MonDKP_DKPTable[s[1][1]].class)
+		    				col = MonDKP:GetCColors(MonDKP:GetTable(MonDKP_DKPTable, true)[s[1][1]].class)
 		    			else
 		    				col = { hex="444444" }
 		    			end
@@ -604,9 +604,9 @@ function MonDKP:LootHistory_Update(filter)				-- if "filter" is included in call
 		    	for j=1, #MonDKP:GetTable(MonDKP_Loot, true) do
 		    		if MonDKP:GetTable(MonDKP_Loot, true)[j]["loot"] == itemToLink and LootTable[i].date ~= MonDKP:GetTable(MonDKP_Loot, true)[j].date and not MonDKP:GetTable(MonDKP_Loot, true)[j].deletedby and not MonDKP:GetTable(MonDKP_Loot, true)[j].deletes then
 		    			local col;
-		    			local s = MonDKP:Table_Search(MonDKP_DKPTable, MonDKP:GetTable(MonDKP_Loot, true)[j].player)
+		    			local s = MonDKP:Table_Search(MonDKP:GetTable(MonDKP_DKPTable, true), MonDKP:GetTable(MonDKP_Loot, true)[j].player)
 		    			if s then
-		    				col = MonDKP:GetCColors(MonDKP_DKPTable[s[1][1]].class)
+		    				col = MonDKP:GetCColors(MonDKP:GetTable(MonDKP_DKPTable, true)[s[1][1]].class)
 		    			else
 		    				col = { hex="444444" }
 		    			end
@@ -625,7 +625,7 @@ function MonDKP:LootHistory_Update(filter)				-- if "filter" is included in call
 				    local del_date1, del_date2, del_date3 = strsplit("/", strtrim(strsub(del_date, 1, 8), " "))
 			    	local s = MonDKP:Table_Search(MonDKP_DKPTable, delOfficer, "player")
 			    	if s then
-			    		col = MonDKP:GetCColors(MonDKP_DKPTable[s[1][1]].class)
+			    		col = MonDKP:GetCColors(MonDKP:GetTable(MonDKP_DKPTable, true)[s[1][1]].class)
 			    	else
 			    		col = { hex="444444"}
 			    	end
